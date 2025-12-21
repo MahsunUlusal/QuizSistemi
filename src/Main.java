312f862c4c665e0bf9085113e44493c4d63733c2
Mahsun Ulusal <ulusalmahsun56@gmail.com>
2025-12-21 17:52:54 +0300

    Classlarda değişiklikler
    User abstract classı oluşturuldu. Teacher, Student, Admin classları bu classı kalıtım aldı. Her kullanıcı için arayüzler eklendi.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,29 @@
+//Bilgilerimi görüntüle eklenebilir
+//Her adımda geri gitme ekle
+
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 	
+	}
+	public static void logIO() {
+		String userName;
+		long password;
+		Scanner input = new Scanner(System.in);
+        System.out.println("Kullanıcı adınızı giriniz.");
+        userName = input.nextLine();
+        System.out.println("Şifrenizi giriniz.");
+        password = input.nextLong();
+        
+        if(isExist(userName,password)) {
+        	
+        }
+        else {
+        	System.out.println("\n\nKullanıcı bulunamadı! Kullanıcı adınızı ya da şifreniz yanlış olablir.\n\n");
+        	logIO();
+        }
 	}
 
 }
