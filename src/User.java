a49c556e260cfcf4ed11e0bf8180527e1b8b6060
Mahsun Ulusal <ulusalmahsun56@gmail.com>
2025-12-24 01:48:36 +0300

    User classda dosya işlemleri
    user sınıfında kullanıcılar ile ilgili yapılacak dosya işlemleri güncellendi ve tamamlandı.

--- src/User.java
+++ src/User.java
@@ -5,7 +5,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
-abstract class User {
+abstract class User implements FileOp{
 	String name,sName,userName;
 	long password;
 	
@@ -63,7 +63,7 @@ abstract class User {
 		       System.out.println("2) Soy ismi");
 		       System.out.println("3) Kullanıcı ismi");
 		       System.out.println("4) Şifresi");
-		       	if(User.show(userName,password,role)=="Teacher") {
+		       	if(User.show(userName,password,"role")=="Teacher") {
 		       		System.out.println("5) Girdiği ders");
 		       		System.out.println("6) Geri");
 		       	}
@@ -80,7 +80,7 @@ abstract class User {
 		             
 		          	 case 1:
 		          		 System.out.println("Kullanıcının ismini giriniz.");
-		          		 String name = input.nextLine();
+		          		 String newName = input.nextLine();
 		          		 User.change(userName,password,"name",newName);
 		          		 check = false;
 		          		 break;
@@ -108,7 +108,7 @@ abstract class User {
 		                break;
 		                
 		             case 5:
-		            	 if(User.show(userName,password,role)=="Teacher") {
+		            	 if(User.show(userName,password,"role")=="Teacher") {
 				          		System.out.println("Ders ismini giriniz.");
 				    			String newSubject = input.nextLine();
 				    			User.change(userName,password,"subject",newSubject);
@@ -148,22 +148,186 @@ abstract class User {
 	        			 
 	 					if(splits[0].equals(userName) && splits[1].equals(passwordString)) {
 	 						
-	 						if(user.show((userName,password,role)=="Teacher"))
+	 						if(User.show(userName,password,"role")=="Teacher") {
 	 							
 	 							switch(target) {
 	 						
 	 							case "name":
-	 								bw.write( splits[0] +";"+ splits[1] +";"+ newTarget +";"+ splits[3] +";"+ splits[4] +";"+ splits[5] "\n");
+	 								bw.write( splits[0] +";"+ splits[1] +";"+ newTarget +";"+ splits[3] +";"+ splits[4] +";"+ splits[5] +"\n");
+	 								System.out.println("Başarıyla değiştirildi.");
+	 								break;
+	 							
+	 							case "sName":
+	 								bw.write( splits[0] +";"+ splits[1] +";"+ splits[2] +";"+ newTarget +";"+ splits[4] +";"+ splits[5] +"\n");
+	 								System.out.println("Başarıyla değiştirildi.");
+	 								break;
+	 								
+	 							case "userName":
+	 								bw.write( newTarget +";"+ splits[1] +";"+ splits[2] +";"+ splits[3] +";"+ splits[4] +";"+ splits[5] +"\n");
+	 								System.out.println("Başarıyla değiştirildi.");
+	 								break;
+	 								
+	 							case "password":
+	 								bw.write(splits[0] +";"+ newTarget +";"+ splits[2] +";"+ splits[3] +";"+ splits[4] +";"+ splits[5] +"\n");
+	 								System.out.println("Başarıyla değiştirildi.");
+	 								break;
+	 								
+	 							case "subject":
+	 								bw.write(splits[0] +";"+ splits[1]  +";"+ splits[2] +";"+ splits[3] +";"+ newTarget +";"+ splits[5] +"\n");
+	 								System.out.println("Başarıyla değiştirildi.");
+	 								break;
+	 								
+	 							}
+	 							bw.newLine();
 	 						}
-	 						}
+	 						else {
+	 							switch(target) {
+		 						
+	 							case "name":
+	 								bw.write( splits[0] +";"+ splits[1] +";"+ newTarget +";"+ splits[3] +";"+ splits[4] +"\n");
+	 								System.out.println("Başarıyla değiştirildi.");
+	 								break;
+	 							
+	 							case "sName":
+	 								bw.write( splits[0] +";"+ splits[1] +";"+ splits[2] +";"+ newTarget +";"+ splits[4] +"\n");
+	 								System.out.println("Başarıyla değiştirildi.");
+	 								break;
+	 								
+	 							case "userName":
+	 								bw.write( newTarget +";"+ splits[1] +";"+ splits[2] +";"+ splits[3] +";"+ splits[4] +"\n");
+	 								System.out.println("Başarıyla değiştirildi.");
+	 								break;
+	 								
+	 							case "password":
+	 								bw.write(splits[0] +";"+ newTarget +";"+ splits[2] +";"+ splits[3] +";"+ splits[4] +"\n");
+	 								System.out.println("Başarıyla değiştirildi.");
+	 								break;
+	 								
+	 							}
+	 							bw.newLine();
+	 						}		
 	 				}
 
+	 					else {
+	 						if(User.show(userName,password,"role")=="Teacher"){
+	 							bw.write(splits[0] +";"+ splits[1]  +";"+ splits[2] +";"+ splits[3] +";"+ splits[4] +";"+ splits[5] +"\n");
+	 							bw.newLine();
+	 						}
+	 						else {
+	 							bw.write(splits[0] +";"+ splits[1] +";"+ splits[2] +";"+ splits[3] +";"+ splits[4] +"\n");
+	 							bw.newLine();
+	 						}
+	 					}
 	        	 }
-	        }
-	        
-	        
-			
-
+	        }   	 
 		}
+	        catch (IOException e) {
+            System.out.println("Hata: " + e.getMessage());
+	        }
+	        users.delete();
+	        newUsers.renameTo(users);
+	}
+		
+		public static String show (String userName, long password, String target) {
+			
+			try {
+				File users = new File("users.txt");
+				Scanner fileInput = new Scanner(users);
+		
+				while(fileInput.hasNextLine()) {
+					String line = fileInput.nextLine();
+					String[] splits = line.split(";");
+			
+					String passwordString = Long.toString(password);
+			
+					if(splits.length==5 || splits.length==6) {
+						if(splits[0].equals(userName) && splits[1].equals(passwordString)) {
+							
+							switch(target) {
+							
+							case "subject":
+								if(splits.length==6) {
+									fileInput.close();
+									return splits[4];
+								}
+								else {
+									System.out.println("Kullanıcı öğretmen değil!");
+									fileInput.close();
+									return null;
+								}
+				
+							case "name":
+								fileInput.close();
+								return splits[2];
+							
+							case "sName":
+								fileInput.close();
+								return splits[3];
+								
+							case "role":
+								if(splits.length==5) {
+									fileInput.close();
+									return splits[4];
+								}
+								else {
+									fileInput.close();
+									return splits[5];
+								}
+								
+							default:
+								System.out.println("Hata!");
+								fileInput.close();
+								return null;
+							}
+							}
+					}
+				}
+			}
+			catch (FileNotFoundException e) {
+				return null;
+			}
+			System.out.println("Hata!");
+			return null;
+		}
+		
+	public static void remove(String userName,long password) {
+		
+		File users = new File("Users.txt");
+        File newUsers = new File("temp.txt");
+		
+        try (BufferedReader br = new BufferedReader(new FileReader(users)); BufferedWriter bw = new BufferedWriter(new FileWriter(newUsers))){
+        	String line;
+        	String passwordString = Long.toString(password);
+        	
+        	 while ((line = br.readLine()) != null) {
+        		 String[] splits = line.split(";");
+        		 
+        		 if(splits.length >=5) {
+        			 
+ 					if(splits[0].equals(userName) && splits[1].equals(passwordString)) {
+ 						bw.newLine();
+ 						continue;
+ 						}
+
+ 					else {
+ 						if(User.show(userName,password,"role")=="Teacher"){
+ 							bw.write(splits[0] +";"+ splits[1]  +";"+ splits[2] +";"+ splits[3] +";"+ splits[4] +";"+ splits[5] +"\n");
+ 							bw.newLine();
+ 						}
+ 						else {
+ 							bw.write(splits[0] +";"+ splits[1] +";"+ splits[2] +";"+ splits[3] +";"+ splits[4] +"\n");
+ 							bw.newLine();
+ 						}
+ 					}
+        	 }
+        }   	 
+	}
+        catch (IOException e) {
+        System.out.println("Hata: " + e.getMessage());
+        }
+        users.delete();
+        newUsers.renameTo(users);	
+	}
 	
+		
 }
